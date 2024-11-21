3c4af3fe18ab7fc0d033c821ac86b1c068e9976b
akiko <akikomoore8@gmail.com>
2024-11-21 20:45:03 +0300

    tutorial_LocLib 8-9

--- LocalLibrary/locallibrary/catalog/models.py
+++ LocalLibrary/locallibrary/catalog/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.urls import reverse
 import uuid
 from datetime import date
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -19,33 +21,34 @@ class Genre(models.Model):
         return reverse('genre_detail', args=[str(self.id)])
 
 
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
 
-
     def get_absolute_url(self):
-
         return reverse('book-detail', args=[str(self.id)])
 
     def display_genre(self):
-        return ', '.join([genre.name for genre in self.genre.all()[:3 ]])
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
     display_genre.short_description = 'Genre'
 
 
 class BookInstance(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -58,13 +61,15 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
-
-
+        permissions = (("can_mark_returned", "set book as returned"),)
     def __str__(self):
+        return '%s (%s)' % (self.id, self.book.title)
 
-        return '%s (%s)' % (self.id,self.book.title)
-
-
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 class Author(models.Model):
@@ -74,20 +79,19 @@ class Author(models.Model):
     date_of_death = models.DateField('Died', null=True, blank=True)
 
     def get_absolute_url(self):
-
         return reverse('author-detail', args=[str(self.id)])
 
-
     def __str__(self):
-
         return '%s, %s' % (self.last_name, self.first_name)
+
 
 class Language(models.Model):
     name = models.CharField(max_length=200,
                             unique=True,
                             help_text="Enter natural language(e.g. English)")
+
     def get_absolute_url(self):
         return reverse('language-detail', args=[str(self.id)])
-    
+
     def __str__(self):
         return self.name
--- LocalLibrary/locallibrary/catalog/views.py
+++ LocalLibrary/locallibrary/catalog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre, Language
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+from .forms import RenewBookForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 def index(request):
@@ -40,3 +49,60 @@ class AuthorListView(generic.ListView):
     paginate_by = 2
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 2
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class MyView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'catalog.can_mark_returned'
+
+def renew_book_librarian(request, pk):
+    book_inst = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+
+        form = RenewBookForm(request.POST)
+
+        # Проверка валидности данных формы:
+        if form.is_valid():
+            # Обработка данных из form.cleaned_data
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+            return HttpResponseRedirect(reverse('all-borrowed') )
+
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial={'date_of_death':'12/10/2016',}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name','last_name','date_of_birth','date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['title','author','author','summary','genre','language']
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
